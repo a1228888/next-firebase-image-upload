0b14b510aa5b6c1958678fc1bf8ca519548ddec6
a1228888 <1228056611@qq.com>
2025-06-12 18:12:48 +0900

    fix: update supabase config

--- src/app/supabaseConfig.js
+++ src/app/supabaseConfig.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://qrnngqrsyeiaqhyybhyt.supabase.co"   
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFybm5ncXJzeWVpYXFoeXliaHl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MDg0MzUsImV4cCI6MjA2NTI4NDQzNX0.22XB5Wa9ly1_Kg0N7VZrINHaXCyShoJYgBUnHcWQqLc"
+const supabaseUrl = 'https://qrnngqrsyeiaqhyybhyt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFybm5ncXJzeWVpYXFoeXliaHl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MDg0MzUsImV4cCI6MjA2NTI4NDQzNX0.22XB5Wa9ly1_Kg0N7VZrINHaXCyShoJYgBUnHcWQqLc'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
